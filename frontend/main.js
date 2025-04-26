1ee4818ecabfd820d1f9030453c389c67e4ce334
Zyres <angaryacine65@gmail.com>
2025-04-26 23:55:07 +0200

    front ya chbab

--- frontend/main.js
+++ frontend/main.js
@@ -7,25 +7,25 @@ document.addEventListener("DOMContentLoaded", async () => {
     let htmlBody = '<div class="m-3"></div>';
     data.forEach(datum => {
         console.log(`=== ${datum.section} ===`);
-        htmlBody += `<div class="shadow is-flex is-justify-content-center mx-5" style="background-color: #FFE66D; padding: 1rem; margin-bottom: 1.5rem; border-radius: 20px; font-weight: bold;"><p>=== ${datum.section} ===</p></div>`;
+        htmlBody += `<div class="shadow is-flex is-justify-content-center mx-6" style="background-color:rgb(185, 189, 62); padding: 1rem; margin-bottom: 1.5rem; border-radius: 20px; font-weight: bold; font-size: large; color: #325947;"><p style="">=== ${datum.section} ===</p></div><div class="is-flex is-justify-content-center is-align-items-center"><div>`;
 
         for (const [label, type] of Object.entries(datum.fields)) {
             if (Array.isArray(type)) {
                 console.log(`${label}: [${type.join(', ')}]`);
 
-                htmlBody += `<div class="is-flex is-justify-content-center"><div class="mx-4"><label>${label} :</label></div> <div class="select custom-select"><select>`;
+                htmlBody += `<div class="is-flex"><div class="mt-2 mx-3" style="width: 330px;"><label style="font-size: large;">${label} :</label></div> <div class="is-flex is-justify-content-center"><div class="select custom-select"><select style="width: 240px;">`;
                 for (const index in type) {
                     htmlBody += `<option>${type[index]}</option>`;
                 }
 
-                htmlBody += "</select></div></div> <br>";
+                htmlBody += "</select></div></div></div> <br>";
             }
             else {
                 console.log(`${label}: ${type}`);
-                htmlBody += `<div class="is-flex is-justify-content-center"><div class="mx-4"><label>${label} :</label></div> <input class="input custom-input w" placeholder="${label}" type="${type}"></div> <br>`;
+                htmlBody += `<div class="is-flex"><div class="mt-2 mx-3 wid"><label style="font-size: large;">${label} :</label></div> <div class="is-flex is-justify-content-center wid"><input class="input custom-input w" placeholder="${label}" type="${type}"></div></div> <br>`;
             }
         }
-        htmlBody += "<br>";
+        htmlBody += "</div></div><br>";
     });
 
     document.getElementById("user").innerHTML = htmlBody;
